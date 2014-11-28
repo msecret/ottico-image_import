14ac264623d71455bc087f0631101c8e41253276
Marco Secreto <msegreto@miceover.com>
2014-11-27 22:07:26 -0800

    Finally fixed promise implementation
    The unknown stack trace is a very large problem.

--- src/img_processor.ts
+++ src/img_processor.ts
@@ -4,34 +4,22 @@ import fs = require('fs');
 import util = require('util');
 
 import args = require('./args');
-import Promise = require('tspromise');
-import Q = require('q');
+import Promise = require('bluebird');
 
-var fs_readdir = Q.denodeify(fs.readdir),
-    fs_readFile = Q.denodeify(fs.readFile);
+var fs_readdir = Promise.promisify(fs.readdir),
+    fs_readFile = Promise.promisify(fs.readFile);
 
 export function getFiles(params: args.IArgs): Promise<any> {
-  var dirs = fs_readdir(params.dirName);
+  var files = fs.readdirSync(params.dirName);
+  var i: number,
+      ilen: number,
+      file: string,
+      filePromises = [];
 
-  return dirs.then(function(files: string[]) {
-    var i: number,
-        ilen: number,
-        file: string,
-        filePromises = [];
+  for (i = 0, ilen = files.length; i < ilen; i++) {
+    file = params.dirName + files[i];
+    filePromises.push(fs_readFile(file));
+  }
 
-    for (i = 0, ilen = files.length; i < ilen; i++) {
-      file = params.dirName + files[i];
-      filePromises.push(fs_readFile(files));
-    }
-
-    filePromises[0].then(function(thing) {
-      console.log('alsjkfaslkjdfal;skjdfalksdjaf;ls');
-      console.log(thing);
-    }, function(err) {
-      console.log('err');
-      console.log(err);
-    });
-
-    return Promise.all(filePromises);
-  });
+  return Promise.all(filePromises);
 }
